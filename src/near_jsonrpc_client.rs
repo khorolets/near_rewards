e4f278cc02becbd49eca0777664eafe2822b2898
Bohdan Khorolets <bogdan@khorolets.com>
2021-05-10 20:37:11 +0300

    Add info about epoch progress to the top of the table (#3)

--- src/near_jsonrpc_client.rs
+++ src/near_jsonrpc_client.rs
@@ -1,15 +1,21 @@
 use serde_json::json;
 
-use crate::primitives::{Response, AccountInPoolResult, AccountInPoolResponse, ViewAccountResponse};
+use crate::primitives::{
+    AccountInPoolResponse, AccountInPoolResult, Block, BlockResponse, Response, Status,
+    StatusReponse, Validators, ValidatorsResponse, ViewAccountResponse,
+};
 
-pub(crate) async fn get_locked_amount(account_id: String) -> Result<u128, reqwest::Error> {
+pub(crate) async fn get_locked_amount(
+    account_id: String,
+    block_height: u64,
+) -> Result<u128, reqwest::Error> {
     let params = json!({
         "jsonrpc": "2.0",
         "id": "dontcare",
         "method": "query",
         "params": json!({
             "request_type": "call_function",
-            "finality": "final",
+            "block_id": block_height,
             "account_id": account_id,
             "method_name": "get_locked_amount",
             "args_base64": ""
@@ -28,14 +34,17 @@ pub(crate) async fn get_locked_amount(account_id: String) -> Result<u128, reqwes
     Ok(body.result.get_amount())
 }
 
-pub(crate) async fn get_liquid_owners_balance(account_id: String) -> Result<u128, reqwest::Error> {
+pub(crate) async fn get_liquid_owners_balance(
+    account_id: String,
+    block_height: u64,
+) -> Result<u128, reqwest::Error> {
     let params = json!({
         "jsonrpc": "2.0",
         "id": "dontcare",
         "method": "query",
         "params": json!({
             "request_type": "call_function",
-            "finality": "final",
+            "block_id": block_height,
             "account_id": account_id,
             "method_name": "get_liquid_owners_balance",
             "args_base64": ""
@@ -54,20 +63,23 @@ pub(crate) async fn get_liquid_owners_balance(account_id: String) -> Result<u128
     Ok(body.result.get_amount())
 }
 
-pub(crate) async fn get_account_in_pool(account_id: String, pool_account_id: String) -> Result<AccountInPoolResult, reqwest::Error> {
+pub(crate) async fn get_account_in_pool(
+    account_id: String,
+    pool_account_id: String,
+    block_height: u64,
+) -> Result<AccountInPoolResult, reqwest::Error> {
     let params = json!({
         "jsonrpc": "2.0",
         "id": "dontcare",
         "method": "query",
         "params": json!({
             "request_type": "call_function",
-            "finality": "final",
+            "block_id": block_height,
             "account_id": pool_account_id,
             "method_name": "get_account",
             "args_base64": base64::encode(json!({"account_id": account_id}).to_string()),
         })
     });
-
 
     let client = reqwest::Client::new();
     let res = client
@@ -78,20 +90,23 @@ pub(crate) async fn get_account_in_pool(account_id: String, pool_account_id: Str
 
     let body: AccountInPoolResponse = res.json().await?;
 
-
-    let account_in_pool: AccountInPoolResult = serde_json::from_slice(&body.result.result[..]).unwrap();
+    let account_in_pool: AccountInPoolResult =
+        serde_json::from_slice(&body.result.result[..]).unwrap();
 
     Ok(account_in_pool)
 }
 
-pub(crate) async fn get_native_balance(account_id: String) -> Result<u128, reqwest::Error> {
+pub(crate) async fn get_native_balance(
+    account_id: String,
+    block_height: u64,
+) -> Result<u128, reqwest::Error> {
     let params = json!({
         "jsonrpc": "2.0",
         "id": "dontcare",
         "method": "query",
         "params": json!({
             "request_type": "view_account",
-            "finality": "final",
+            "block_id": block_height,
             "account_id": account_id,
         })
     });
@@ -106,4 +121,67 @@ pub(crate) async fn get_native_balance(account_id: String) -> Result<u128, reqwe
     let body: ViewAccountResponse = res.json().await?;
 
     Ok(body.result.get_amount())
+}
+
+pub(crate) async fn get_status() -> Result<Status, reqwest::Error> {
+    let params = json!({
+        "jsonrpc": "2.0",
+        "id": "dontcare",
+        "method": "status",
+        "params": json!({}),
+    });
+
+    let client = reqwest::Client::new();
+
+    let res = client
+        .post("https://rpc.mainnet.internal.near.org")
+        .json(&params)
+        .send()
+        .await?;
+
+    let body: StatusReponse = res.json().await?;
+
+    Ok(body.result)
+}
+
+pub(crate) async fn get_validators() -> Result<Validators, reqwest::Error> {
+    let params = json!({
+        "jsonrpc": "2.0",
+        "id": "dontcare",
+        "method": "validators",
+        "params": json!({"latest": null}),
+    });
+
+    let client = reqwest::Client::new();
+
+    let res = client
+        .post("https://rpc.mainnet.internal.near.org")
+        .json(&params)
+        .send()
+        .await?;
+
+    let body: ValidatorsResponse = res.json().await?;
+
+    Ok(body.result)
+}
+
+pub(crate) async fn get_block(block_height: u64) -> Result<Block, reqwest::Error> {
+    let params = json!({
+        "jsonrpc": "2.0",
+        "id": "dontcare",
+        "method": "block",
+        "params": json!({"block_id": block_height}),
+    });
+
+    let client = reqwest::Client::new();
+
+    let res = client
+        .post("https://rpc.mainnet.internal.near.org")
+        .json(&params)
+        .send()
+        .await?;
+
+    let body: BlockResponse = res.json().await?;
+
+    Ok(body.result)
 }
--- src/utils/human.rs
+++ src/utils/human.rs
@@ -1,3 +1,7 @@
 pub(crate) fn human(yocto: u128) -> u128 {
     yocto / 1000000000000000000000000
 }
+
+pub(crate) fn current_position_in_epoch(start: u64, current: u64) -> u64 {
+    (current - start) * 100 / crate::EPOCH_LENGTH
+}
