5a9a88d50a415b534f1b7304491d54d58ef6a523
Marcelo Fornet <mfornet94@gmail.com>
2021-08-31 11:34:35 -0400

    0.4.2
    - Print partial updates while data is being downloaded (instead of
    waiting to the end) To do this, use --verbose.
    
    - Add price to the table (usefule on verbose mode)
    
    - Determine pool automatically (if it is a lockup account)
    
    - Sort accounts by key (if specified in accounts.json)

--- src/configs.rs
+++ src/configs.rs
@@ -3,11 +3,13 @@ use clap::Clap;
 /// NEAR Rewards
 /// Checks the rewards of lockup accounts
 #[derive(Clap, Debug)]
-#[clap(version = "0.3.0", author = "Bohdan Khorolets <b@khorolets.com>")]
+#[clap(version = "0.4.2", author = "Bohdan Khorolets <b@khorolets.com>")]
 pub(crate) struct Opts {
     /// Sets a custom near_rewards dir. Defaults to ~/near_rewards
     #[clap(short, long)]
     pub home_dir: Option<std::path::PathBuf>,
     // #[clap(subcommand)]
     // pub subcmd: SubCommand,
+    #[clap(short, long)]
+    pub verbose: bool,
 }
